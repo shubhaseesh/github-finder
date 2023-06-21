420d832b4aeebe41870363b9ebf90f6c0a3c4cf5
shubhaseesh <shubhaseesh09@gmail.com>
2023-06-21 16:59:14 +0530

    added user item component

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="loading"/>
+    </div>
+     
+  );
+};
+
+export default Spinner;
--- src/components/users/UserList.jsx
+++ src/components/users/UserList.jsx
@@ -1,12 +1,43 @@
-import React, { useState } from 'react'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
 
 const UserList = () => {
-    const [users, setUsers] = useState(null)
-  return (
-    <div>
-        Users will come here
-    </div>
-  )
-}
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-export default UserList
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    axios
+      .get(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+        headers: {
+          Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        },
+      })
+      .then((res) => {
+        // console.log(res.data[0]);
+        setUsers(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+  if (isLoading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2">
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
+};
+
+export default UserList;
